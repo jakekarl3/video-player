a22ddc21ea62f05ff5d3b588e242b13b70a32ea7
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-10-12 21:59:53 -0500

    Add mute/unmute function

--- script.js
+++ script.js
@@ -56,6 +56,9 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+let lastVolume = 1;
+let lastVolumeIcon = 'fas fa-volume-up';
+
 // Volume Bar
 function changeVolume(e) {
   let volume = e.offsetX / volumeRange.offsetWidth;
@@ -78,6 +81,26 @@ function changeVolume(e) {
   } else if (volume === 0) {
     volumeIcon.classList.add('fas', 'fa-volume-off');
   }
+
+  lastVolume = volume;
+}
+
+// Mute/Unmute
+function toggleMute() {
+  // volumeIcon.className = '';
+  if (video.volume || volumeIcon.className === 'fas fa-volume-off') {
+    lastVolume = video.volume;
+    lastVolumeIcon = volumeIcon.classList.value;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.className = 'fas fa-volume-mute';
+    volumeIcon.setAttribute('title', 'Unmute');
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.className = lastVolumeIcon;
+    volumeIcon.setAttribute('title', 'Mute');
+  }
 }
 
 // Change Playback Speed -------------------- //
@@ -93,3 +116,4 @@ video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
